6cde49b8effcf1c6384d14f8e1d4d89b0af69bc2
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2022-06-30 10:07:32 +0300

    task-07-fixed

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,8 @@
 const fontInputEl = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
 
+fontInputEl.addEventListener('input', onInputSize);
+
 function onInputSize(event) {
     textEl.style.fontSize = event.currentTarget.value + 'px';
 };
-
-fontInputEl.addEventListener('input', onInputSize);
